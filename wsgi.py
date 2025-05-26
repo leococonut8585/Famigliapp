6ccd25e2f49672fd9c9220e03dc144f2bca220f8
leococonut8585 <youforgotit85@gmail.com>
2025-05-26 23:15:18 +0900

    Add scheduler for Lezzione feedback reminders

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,9 @@
 
 from app import create_app
 from app.resoconto.tasks import start_scheduler
+from app.lezzione.tasks import start_scheduler as start_lezzione_scheduler
 
 app = create_app()
 start_scheduler()
+start_lezzione_scheduler()
 
