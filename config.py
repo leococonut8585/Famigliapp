1cb9bcc9e1d4d152690adbc344ab598518e7b41a
leococonut8585 <youforgotit85@gmail.com>
2025-05-27 14:02:20 +0900

    Add new admin and user accounts

--- config.py
+++ config.py
@@ -2,6 +2,16 @@ USERS = {
     "admin": {"password": "adminpass", "role": "admin", "email": "admin@example.com"},
     "user1": {"password": "user1pass", "role": "user", "email": "user1@example.com"},
     "user2": {"password": "user2pass", "role": "user", "email": "user2@example.com"},
+    "leo": {"password": "leococonut85", "role": "admin", "email": "leo@example.com"},
+    "lady": {"password": "shihopoteto850", "role": "admin", "email": "lady@example.com"},
+    "raito": {"password": "raitocetriolo851", "role": "user", "email": "raito@example.com"},
+    "hitomi": {"password": "hitomioniku852", "role": "user", "email": "hitomi@example.com"},
+    "sara": {"password": "sarapasta853", "role": "user", "email": "sara@example.com"},
+    "giun": {"password": "giunamazake854", "role": "user", "email": "giun@example.com"},
+    "nanchan": {"password": "shojigruto855", "role": "user", "email": "nanchan@example.com"},
+    "hachi": {"password": "hachipakuchi856", "role": "user", "email": "hachi@example.com"},
+    "kie": {"password": "kieriso857", "role": "user", "email": "kie@example.com"},
+    "gumi": {"password": "kumigumi858", "role": "user", "email": "gumi@example.com"},
 }
 
 POINTS_FILE = "points.json"
