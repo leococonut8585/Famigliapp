c38a07ef42752538149264b4f0f664988de5fb17
leococonut8585 <youforgotit85@gmail.com>
2025-05-27 10:46:41 +0900

    Start additional schedulers in wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,16 @@
 from app import create_app
 from app.resoconto.tasks import start_scheduler
 from app.lezzione.tasks import start_scheduler as start_lezzione_scheduler
+from app.intrattenimento.tasks import (
+    start_scheduler as start_intrattenimento_scheduler,
+)
+from app.principessina.tasks import (
+    start_scheduler as start_principessina_scheduler,
+)
 
 app = create_app()
 start_scheduler()
+start_intrattenimento_scheduler()
 start_lezzione_scheduler()
+start_principessina_scheduler()
 
