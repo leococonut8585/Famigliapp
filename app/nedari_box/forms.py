5a3ffe1e4efec25a7d6066aaf7426b765438a455
leococonut8585 <youforgotit85@gmail.com>
2025-05-27 17:11:31 +0900

    Use checkboxes for Nedari targets

--- app/nedari_box/forms.py
+++ app/nedari_box/forms.py
@@ -1,11 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, SelectMultipleField, SelectField
+from wtforms import (
+    TextAreaField,
+    SubmitField,
+    SelectMultipleField,
+    SelectField,
+    widgets,
+)
 from wtforms.validators import DataRequired
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 
 class NedariForm(FlaskForm):
     body = TextAreaField('内容', validators=[DataRequired()])
-    targets = SelectMultipleField('対象ユーザー', validators=[DataRequired()], coerce=str)
+    targets = MultiCheckboxField('対象ユーザー', validators=[DataRequired()], coerce=str)
     visibility = SelectField(
         '公開範囲',
         choices=[('all', '全員閲覧可能'), ('admins', 'シニョーレとレディのみ閲覧可能')]
